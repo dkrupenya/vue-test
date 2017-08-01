86d130135a68a5b9e49c774ac3e04adeb2911628
Dmitry Krupenya <Dmitry.Krupenya@t-systems.ru>
2017-08-01 11:46:34 +0300

    add tslint rules

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router/index'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router/index';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  el: '#app',
   router,
+  el: '#app',
   template: '<App/>',
-  components: { App }
+  components: { App },
 });
